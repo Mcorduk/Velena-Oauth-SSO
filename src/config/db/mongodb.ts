1344df741d9e6eec41877808b4601e55a46c38bf
Mcorduk <om.corduk@gmail.com>
2024-02-03 14:30:13 +0300

    fix: fix mdb connection file, returns mongoose

--- src/config/db/mongodb.ts
+++ src/config/db/mongodb.ts
@@ -1,29 +1,26 @@
 import mongoose, { Mongoose } from 'mongoose';
-import logger from '../log.ts';
+import logger from '../log';
 
 const log = logger('models');
 
-export async function connectToMongoDB(
-  mongoUrl: string = process.env.MONGO_DB_URL || '',
-  options?: mongoose.ConnectOptions,
-): Promise<Mongoose> {
-  const mongoOpts: mongoose.ConnectOptions = {
-    useNewUrlParser: true,
-    ...options,
-  };
-
+export async function connectToMongoDB(): Promise<Mongoose> {
   try {
-    const connection = await mongoose.connect(mongoUrl, mongoOpts);
-    log.debug(`Connected to MongoDB at ${connection.connection.host}`);
+    const mongoUrl: string = process.env.MONGO_DB_URL || '';
+
+    // Connect to MongoDB and assign the connection object to a variable
+    await mongoose.connect(mongoUrl);
+
+    // Access the connection object using `mongoose.connection`
+    const connection = mongoose.connection;
 
     connection.on('connected', () => {
-      log.debug('MongoDB connection established');
+      log.debug(`Connected to MongoDB at ${connection.host}`);
     });
 
     connection.on('disconnected', () => {
       log.warn('MongoDB connection disconnected');
       // Attempt reconnection with a slight delay to avoid rapid reconnects
-      setTimeout(() => connectToMongoDB(mongoUrl, options), 5000);
+      setTimeout(() => connectToMongoDB(), 5000);
     });
 
     connection.on('error', (err: Error) => {
@@ -31,9 +28,13 @@ export async function connectToMongoDB(
       // Handle errors appropriately, potentially retrying or notifying users
     });
 
-    return connection;
-  } catch (error) {
-    log.error('Error connecting to MongoDB:', error.message);
-    throw error; // Rethrow to allow higher-level error handling
+    return mongoose;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      log.error('Error connecting to MongoDB:', error.message);
+      throw error; // Rethrow to allow higher-level error handling
+    } else {
+      throw new Error('Unknown error occurred while connecting to MongoDB');
+    }
   }
 }
