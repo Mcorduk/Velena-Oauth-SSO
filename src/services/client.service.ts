7fa4c65f171e6ad630354c6164bfb910357c8d3b
Mcorduk <om.corduk@gmail.com>
2024-02-05 20:34:01 +0300

    fix: fixed type

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -1,8 +1,9 @@
 import Client from '../models/client.model';
+import clientType from '../types/models/clientType';
 import { generateClientId, generateClientSecret } from '../utils/crypto';
 
 export class ClientService {
-  async createClient(name: string, redirectUrl: string, description?: string): Promise<Client> {
+  async createClient(name: string, redirectUrl: string, description?: string): Promise<clientType> {
     const clientId = generateClientId();
     const clientSecret = generateClientSecret();
     const newClient = new Client({ clientId, clientSecret, name, redirectUrl, description });
@@ -10,7 +11,7 @@ export class ClientService {
     return newClient;
   }
 
-  async findClientById(clientId: string): Promise<Client | null> {
+  async findClientById(clientId: string): Promise<clientType | null> {
     return Client.findById(clientId);
   }
 
