5c4bb25aaed1ef354357a3cf53ffeff544b60664
Mcorduk <om.corduk@gmail.com>
2024-02-13 10:54:34 +0300

    feat: create the routers

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,9 +2,8 @@ import express, { NextFunction, Request, Response, Router } from 'express';
 import * as auth from './auth';
 import * as me from './me';
 import * as oauth from './oauth2';
-const router: Router = express.Router();
 
-export const router = express.Router();
+const router: Router = express.Router();
 
 router.use('/api/auth', auth.router);
 router.use('/api/me', me.router);
