8c3cf427569878eba377e5d59e816a1e80b85449
Mcorduk <om.corduk@gmail.com>
2024-03-01 20:18:35 +0300

    Add routes for main auth actions

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,3 +1,23 @@
 import express from 'express';
 
 export const router = express.Router();
+
+router.post('/login', (req, res) => {
+  // ...
+});
+
+router.post('/register', (req, res) => {
+  // ...
+});
+
+router.post('/logout', (req, res) => {
+  // ...
+});
+
+router.post('/forgot-password', (req, res) => {
+  // ...
+});
+
+router.post('/reset-password', (req, res) => {
+  // ...
+});
